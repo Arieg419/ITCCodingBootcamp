d6641583a9f352d2a8b3e09e6e9b8e62b8f407b0
Omer Goldberg <arieg419@gmail.com>
2016-12-08 22:34:59 +0200

    another one

--- InterviewQuestions/mergeMeetingsQuestion.js
+++ InterviewQuestions/mergeMeetingsQuestion.js
@@ -33,4 +33,3 @@
 
 // Do not assume the meetings are in order. The meeting times are coming from multiple teams.
 
-// Write a solution that's efficient even when we can't put a nice upper bound on the numbers representing our time ranges. Here we've simplified our times down to the number of 30-minute slots past 9:00 am. But we want the function to work even for very large numbers, like Unix timestamps. In any case, the spirit of the challenge is to merge meetings where startTime and endTime don't have an upper bound.
