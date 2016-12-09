4917c9fa0f5ef3e5baa0798f9f6a9cace0aad7a8
Omer Goldberg <arieg419@gmail.com>
2016-12-09 20:15:25 +0200

    rectangle intersection q

---
+++ InterviewQuestions/rectangleLove.js
@@ -0,0 +1,24 @@
+// Tinder love scientists have devised a way to represent dating profiles as rectangles on a 
+// two-dimensional plane. Write an algorithm to find the intersection of two 
+// users' love rectangles. 
+
+// Write a function to find the rectangular intersection of two given rectangles.
+
+// As with the example above, love rectangles are always "straight" and never "diagonal." 
+// More rigorously: each side is parallel with either the x-axis or the y-axis.
+
+// They are defined as objects like this:
+
+//   var rectangle = {
+
+//     // coordinates of bottom-left corner
+//     leftX: 1,
+//     bottomY: 5,
+
+//     // width and height
+//     width: 10,
+//     height: 4,
+
+// };
+
+// Your output rectangle should use this format as well.
