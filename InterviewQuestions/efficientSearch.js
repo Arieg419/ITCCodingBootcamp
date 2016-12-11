0f75808830d3964f3758ac228c4910de16ea3799
Omer Goldberg <arieg419@gmail.com>
2016-12-11 11:40:22 +0200

    added efficientSearch.js

---
+++ InterviewQuestions/efficientSearch.js
@@ -0,0 +1,17 @@
+/*
+
+Given a sorted array of size n, write the quickest algorithm possible for finding a given
+integer.
+
+Example: 
+
+Given 
+array: var arr = [1,2,3,4,5,6,7,8,9];
+number: 5
+
+Write a function that returns the index of 5 in the array, if it exists. In our example
+the index returned would be 4.
+
+The naive solution is scanning the whole array. Can we do better?
+		
+*/
