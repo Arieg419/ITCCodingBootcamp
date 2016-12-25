daa3ea16778d074986d86397f13f8dab965c76ab
Omer Goldberg <arieg419@gmail.com>
2016-12-25 10:56:36 +0200

    updated app.js

--- BuildAFramework/Exercise/app.js
+++ BuildAFramework/Exercise/app.js
@@ -1,27 +1,27 @@
 console.log("Running Driver...");
 
 // Should create Jackson Instance
-var one = $Jackson.createJacksonInstance("#one");
-console.log("new jackson obj is ", one);
+var element = $Jackson.createJacksonInstance("#element");
+console.log("new jackson obj is ", element);
 
-// Should set DOM node with ID of 'one' innerHTML 
-one.html("Hello world!");
+// Should set DOM node with ID of 'element' innerHTML 
+element.html("Hello world!");
 
-// Should get innerHTML of DOM node with #one ID
-console.log(one.html());
+// Should get innerHTML of DOM node with #element ID
+console.log(element.html());
 
-// Should remove class 'redClass' from 'one'
-one.removeClass("redClass");
+// Should remove class 'redClass' from 'element'
+element.removeClass("redClass");
 
-// Should add class 'yellowClass' from 'one'
-one.addClass("yellowClass");
+// Should add class 'yellowClass' from 'element'
+element.addClass("yellowClass");
 
 // this will be an array holding every DOM node with class "two"
-var twos = $Jackson.createJacksonInstance(".two");
-console.log("Array of elements with 'two' class ", twos);
+var list = $Jackson.createJacksonInstance(".list");
+console.log("Array of elements with 'two' class ", list);
 
-// Should add attribute alt with value of randomString to twos class
-twos.attr('alt', 'randomString');
+// Should add attribute alt with value of randomString to list class
+list.attr('alt', 'randomString');
 
 
 
