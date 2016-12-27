faccc285b2185d4be95af02ebe22a843292f14f9
Omer Goldberg <arieg419@gmail.com>
2016-12-27 15:32:43 +0200

    updated apple stock q

--- InterviewQuestions/appleStocksQuestion.js
+++ InterviewQuestions/appleStocksQuestion.js
@@ -7,9 +7,38 @@
 // Write an efficient function that takes stockPricesYesterday and returns the best profit 
 // I could have made from 1 purchase and 1 sale of 1 Apple stock yesterday.
 
-// var arr1 = [10, 7, 5, 8, 11, 9, 1] -> max profit is 6. buy at 5 dollars, and sell at 11 dollars.
 
+function getMaxProfit(arr) {
+	var minIdx = 0;
+    var maxIdx = 1;
+    var currMin = 0;
+    var maxProfit = 0;
 
+    if(arr.length < 2) {
+        throw new Error("Need atleast two time periods to be profitable!");
+    }
+    
+    for(var i = 1; i < arr.length; i++) {
 
+        // new current min.
+        if(arr[i] < arr[currMin]) { 
+        	currMin = i;
+        }
+        
+        // new best profit
+        if(arr[maxIdx] - arr[minIdx] < arr[i] - arr[currMin]) {
+                maxIdx = i;
+            	minIdx = currMin;
+        }
 
+    }
 
+    maxProfit  = arr[maxIdx] - arr[minIdx];
+    return maxProfit;
+}
+
+// run your function through some test cases here
+// remember: debugging is half the battle!
+var arr1 = [10, 7, 5, 8, 11, 9, 1];
+var arr2 = [1];
+console.log(getMaxProfit(arr1));
