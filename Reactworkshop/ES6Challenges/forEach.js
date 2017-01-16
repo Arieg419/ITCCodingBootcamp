624d651bfc575bc71bd8e65a144561fcb41393cb
Omer Goldberg <arieg419@gmail.com>
2017-01-16 10:52:33 +0200

    added es6 challenge

---
+++ Reactworkshop/ES6Challenges/forEach.js
@@ -0,0 +1,69 @@
+console.log("***************** COLORS ******************");
+var colors = ['red', 'blue', 'green'];
+
+// Let's move away from this :)
+for(var i = 0; i < colors.length; i++) {
+  console.log(colors[i]);
+}
+
+// Create an array of numbers
+var numbers = [1,2,3,4,5];
+
+// Create a variable to hold the submitvar sum = 0;
+var sum = 0;
+
+for(var i = 0; i < numbers.length; i++) {
+  sum += numbers[i];
+}
+
+console.log("Numbers with named funciton...");
+console.log(sum);
+
+console.log("***************** END OF NUMBERS ******************");
+
+
+console.log("***************** ES6 Challenge ******************");
+
+// ES6 Challenge
+
+/*
+The code below is calling 'savePost' three times, but it is doing so using a for loop.
+This implementation works, but the for loop makes it more challenging to understand the purpose of the function.
+Rather than using a for loop, refactor the code below to instead use the forEach helper.
+*/
+
+
+
+function handlePosts() {
+   function savePost(post) {console.log(post)};
+    var posts = [
+      { id: 23, title: 'Daily JS News' },
+      { id: 52, title: 'Code Refactor City' },
+      { id: 105, title: 'The Brightest Ruby' }
+    ];
+
+    for (var i = 0; i < posts.length; i++) {
+      savePost(posts[i]);
+    }
+}
+console.log("***************** END OF CHALLENGE 1 ******************");
+/*
+The array below contains an array of objects, each of which is a representation of an image.
+Using the forEach helper, calculate the area of each image and store it in a new array called 'areas'.
+The area of an image can be calculated as 'image.height * image.width'.
+*/
+
+console.log("***************** CHALLENGE 2 ******************");
+
+var images = [
+  { height: 10, width: 30 },
+  { height: 20, width: 90 },
+  { height: 54, width: 32 }
+];
+var areas = [];
+
+
+console.log("areas");
+console.log(areas);
+
+console.log("***************** END OF CHALLENGE 2 ******************");
---
+++ Reactworkshop/ES6Challenges/mapHelper.js
@@ -0,0 +1,62 @@
+var numbers = [1,2,3];
+doubledNumbers = [];
+
+for(var i = 0; i < numbers.length; i++) {
+  doubledNumbers.push(numbers[i] * 2);
+}
+
+console.log(doubledNumbers);
+
+console.log("************ REFACTORED CODE MAP DOUBLE NUMBERS CODE *************");
+
+
+console.log(doubled);
+
+var cars = [
+  {model: 'Buick', price: 'cheap'},
+  {model: 'Camaro', price: 'expensive'}
+];
+
+console.log(prices);
+
+ console.log("************** MAP CHALLENGE 1 **************");
+ /*
+ Using map, create a new array that contains the 'height' property of each object.
+ Assign this new array to the variable 'heights'.
+ Don't forget to use the 'return' keyword in the function!
+ */
+
+ var images = [
+   { height: '34px', width: '39px' },
+   { height: '54px', width: '19px' },
+   { height: '83px', width: '75px' },
+ ];
+
+ var heights;
+
+console.log("heights is ");
+ console.log(heights);
+
+ console.log("************** END MAP CHALLENGE 1 **************");
+
+ console.log("************** MAP CHALLENGE 2 **************");
+
+ /*
+Using map, create a new array that contains the distance / time value from each trip.
+In other words, the new array should contain the (distance / time) value.
+Assign the result to the variable 'speeds'.
+ */
+
+ var trips = [
+  { distance: 34, time: 10 },
+  { distance: 90, time: 50 },
+  { distance: 59, time: 25 }
+];
+
+var speeds;
+
+
+console.log("sppeds is ");
+console.log(speeds);
+
+ console.log("************** END MAP CHALLENGE 2 **************");
